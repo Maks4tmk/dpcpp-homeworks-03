24a0cf2c9ce08a3f2e451f0c1e6b8e296d9616ba
Maksim <maks4tmk5423@yandex.ru>
2024-08-07 00:00:55 +0300

    initial commit

---
+++ task_1/task_1.cpp
@@ -0,0 +1,66 @@
+﻿// task_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+
+
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+
+
+class ConsoleLogCommand : public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::cout << message << std::endl;
+    }
+};
+
+
+
+class FileLogCommand : public LogCommand {
+public:
+    FileLogCommand() {
+        m_file.open("log.txt", std::fstream::app);
+        if (!m_file.is_open()) {
+            std::cout << "Не удается открыть файл log.txt!" << std::endl;
+        }
+    }
+
+    ~FileLogCommand() {
+        m_file.close();
+    }
+
+    void print(const std::string& message) override {
+        m_file << message << std::endl;
+    }
+
+private:
+    std::fstream m_file;
+};
+
+
+
+void print(LogCommand& command, const std::string& message) {
+    command.print(message);
+}
+
+
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    ConsoleLogCommand consoleCommand;
+    FileLogCommand fileCommand;
+
+    print(consoleCommand, "Привет консоль");
+    print(fileCommand, "Привет фаил");
+
+    return 0;
+}
+
---
+++ task_2/task_2.cpp
@@ -0,0 +1,114 @@
+﻿// task_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+class Observer {
+public:
+	virtual void Warning(const std::string& message) {};
+	virtual void Error(const std::string& message) {};
+	virtual void FatalError(const std::string& message) {};
+	virtual ~Observer() = default;
+};
+
+class Observable {
+public:
+	void addObserver(Observer* observer) {
+		observers_.push_back(observer);
+	}
+
+	void removeObserver(Observer* observer) {
+		auto it = remove(observers_.begin(), observers_.end(), observer);
+		observers_.erase(it, observers_.end());
+	}
+
+	void warning() {
+		for (auto obs : observers_) {
+			obs->Warning("Warning!");
+		}
+	}
+
+	void error() {
+		for (auto obs : observers_) {
+			obs->Error("Error!");
+		}
+	}
+
+	void fatalError() const {
+		for (auto obs : observers_) {
+			obs->FatalError("Fatal Error!");
+		}
+	}
+
+private:
+	std::vector <Observer*> observers_;
+};
+
+
+class WarningObserver : public Observer {
+public:
+	void Warning(const std::string& message) override {
+		std::cout << "Warning: " << message << "\n";
+	}
+};
+
+class ErrorObserver : public Observer {
+private:
+	std::ofstream write;
+public:
+	ErrorObserver(const std::string& filename) {
+		write.open(filename, std::ofstream::app);
+		if (!write.is_open()) {
+			std::cout << "Can't open the file: " << filename << "!\n";
+		}
+	}
+
+	~ErrorObserver() {
+		write.close();
+	}
+
+	void Error(const std::string& message) override {
+		write << "Error: " << message << "\n";
+	}
+};
+
+class FatalErrorObserver : public Observer {
+private:
+	std::ofstream write;
+public:
+	FatalErrorObserver(const std::string& filename) {
+		write.open(filename, std::ofstream::app);
+		if (!write.is_open()) {
+			std::cout << "Can't open the file: " << filename << "!\n";
+		}
+	}
+
+	~FatalErrorObserver() {
+		write.close();
+	}
+
+	void FatalError(const std::string& message) override {
+		std::cout << "Fatal error: " << message << "\n";
+		write << "Fatal error: " << message << "\n";
+	}
+};
+
+int main() {
+
+	Observable Obs;
+	WarningObserver warning;
+	ErrorObserver error("error_file.txt");
+	FatalErrorObserver fatal("fatal_file.txt");
+
+	Obs.addObserver(&warning);
+	Obs.addObserver(&error);
+	Obs.addObserver(&fatal);
+
+	Obs.warning();
+	Obs.error();
+	Obs.fatalError();
+
+	return 0;
+}
